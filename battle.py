dda5e59266967f3e92c1be47d510f7652bf3ab0f
David Mchedlishvili <38452729+xentellion@users.noreply.github.com>
2022-02-16 20:43:01 +0300

    Refactor init and add aliases

--- battle.py
+++ battle.py
@@ -10,109 +10,117 @@ import discord
 class Battle(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.group(aliases=['init', 'i'])
+    async def init_combat(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("Command can't be invoked without a subcommand")
  
-    @commands.command(name='init')
-    async def combat(self, ctx):
-        await ctx.message.delete()
-        init_command = ctx.message.content[5:].strip()
-        data = init_command.split()
-        if data[0] == 'begin':
-            path = f'{main.folder}{ctx.message.channel.id}.json'
-            try:
-                f = open(path)
-                await ctx.send('There is already a combat in this channel!')
-                f.close()
-                return
-            except FileNotFoundError:
-                print('New Combat Session')
+    @init_combat.command()
+    async def begin(self, ctx):
+        path = f'{main.folder}{ctx.message.channel.id}.json'
+        try:
+            f = open(path)
+            await ctx.send('There is already a combat in this channel!')
+            f.close()
+            return
+        except FileNotFoundError:
+            print('New Combat Session')
 
-            text = "```md\nCurrent initiative: 0 (round 0)\n```"
-            text += '=' * (len(text) - 7) + '\n'
-            message = await ctx.send(text)
-            await message.pin()
-            new_combat = combat_data.Combat(ctx.message.channel.id, message.id)
-            self.save_update(path, new_combat)
-            await ctx.send("""**Справка:** 
-                        !init begin - Начать бой
-                        !init end - Закончить бой
-                        !init add <mod> <name> - Добавить участника
-                        !init remove <name> - Убрать участника
-                        !init next - Передать очередь""")
-            
-        elif data[0] == 'end':
-            path = f'{main.folder}{ctx.message.channel.id}.json'
-            m = await self.get_file_data(ctx, path)
-            await ctx.send('**Are you sure you wnt to stop the combat? Type yes/no**')
-            answers = { 'yes', 'no'}
+        text = "```md\nCurrent initiative: 0 (round 0)\n"
+        text += '=' * (len(text) - 7) + '\n```'
+        message = await ctx.send(text)
+        await message.pin()
+        new_combat = combat_data.Combat(ctx.message.channel.id, message.id)
+        self.save_update(path, new_combat)
+        await ctx.send("""**Справка:** 
+                !init begin - Начать бой
+                !init end - Закончить бой
+                !init add <mod> <name> - Добавить участника
+                !init remove <name> - Убрать участника
+                !init next - Передать очередь""")
 
-            def check(m: discord.Message):
-                return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id and m.content.lower() in answers
+    @init_combat.command()
+    async def end(self, ctx):
+        path = f'{main.folder}{ctx.message.channel.id}.json'
+        m = await self.get_file_data(ctx, path)
+        await ctx.send('**Are you sure you want to stop the combat? Type yes/no**')
+        answers = { 'yes', 'no'}
 
-            try:
-                request_msg = await main.bot.wait_for(event='message', check= check, timeout= 15.0)
-            except asyncio.TimeoutError:
-                await ctx.send('**Time for an answer has ended or the answer is wrong**')
+        def check(m: discord.Message):
+            return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id and m.content.lower() in answers
+        
+        try:
+            request_msg = await main.bot.wait_for(event='message', check= check, timeout= 15.0)
+        except asyncio.TimeoutError:
+            await ctx.send('**Time for an answer has ended or the answer is wrong**')
+        else:
+            if request_msg.content.lower() == 'yes':
+                msg = await ctx.fetch_message(m.message)
+                await msg.delete()
+                os.remove(path)
+                await ctx.send('**End of combat**')
             else:
-                if request_msg.content.lower() == 'yes':
-                    msg = await ctx.fetch_message(m.message)
-                    await msg.delete()
-                    os.remove(path)
-                    await ctx.send('**End of combat**')
-                else:
-                    await ctx.send('**Cancelled**')
-            
-        elif data[0] == 'add':
-            path = f'{main.folder}{ctx.message.channel.id}.json'
-            m = await self.get_file_data(ctx, path)
-            rand = random.randint(1, 20)
-            sign = '+'
-            try:
-                if not data[1].isdigit():
-                    if data[1][1:].isdigit():
-                        sign = data[1][0]
-                        data[1] = data[1][1:]
-                    else:
-                        data.insert(1, 0)
-                sum = eval(str(rand) + sign + str(data[1]))
-                actor = combat_data.Actor(''.join(data[2:]), ctx.message.author.id, sum)       
-            except:
-                await ctx.send('Incorrect input!')
+                await ctx.send('**Cancelled**')
+
+    @init_combat.command()
+    async def add(self, ctx, mod, *args):
+        path = f'{main.folder}{ctx.message.channel.id}.json'
+        m = await self.get_file_data(ctx, path)
+        rand = random.randint(1, 20)
+        sign = '+'
+        args = ' '.join(args)
+        if not mod.isdigit():
+            if mod[1:].isdigit():
+                sign = mod[0]
+                mod = mod[1:]
+            else:
+                args = mod + ' ' + args
+                mod = 0
+        sum = eval(str(rand) + sign + str(mod))
+        actor = combat_data.Actor(''.join(args).strip(), ctx.message.author.id, sum) 
+        m.add_actors(actor.__dict__)
+        if sum >= m.get_current()['initiative'] and len(m.actors) > 1:
+            m.turn += 1
+        self.save_update(path, m)
+        await self.update_message(ctx, m)
+        await ctx.send(f'`{actor.name}` has been added to combat with initiative 1d20 ({rand}) {sign} {mod} = `{sum}`.')
+
+    @init_combat.command()
+    async def remove(self, ctx, name):
+        print(name)
+        if not ctx.message.author.guild_permissions.administrator:
                 return
-            m.add_actors(actor.__dict__)
+        path = f'{main.folder}{ctx.message.channel.id}.json'
+        m = await self.get_file_data(ctx, path)
+        actor = m.get_current()
+        if actor['name'] == name:
+            await ctx.send(f'You can\'t remove characters on their turn!')
+            return
+        try:
+            if m.actors.index(m.get_actor(name)) < m.turn:
+                m.turn -= 1
+            m.remove_actors(name)
+        except ValueError:
+            await ctx.send(f'`{name}` has not been found')
+            return
+        self.save_update(path, m)
+        await self.update_message(ctx, m)
+        await ctx.send(f'`{name}` has been removed from combat')
+
+    @init_combat.command()
+    async def next(self, ctx):
+        path = f'{main.folder}{ctx.message.channel.id}.json'
+        m = await self.get_file_data(ctx, path)
+        actor = m.get_current()
+        if ctx.message.author.id == actor['author'] or ctx.message.author.guild_permissions.administrator:
+            m.next_turn()
+            actor = m.get_current()
             self.save_update(path, m)
             await self.update_message(ctx, m)
-            await ctx.send(f'`{actor.name}` has been added to combat with initiative 1d20 ({rand}) {sign} {data[1]} = `{sum}`.')
-
-        elif data[0] == 'remove':
-            path = f'{main.folder}{ctx.message.channel.id}.json'
-            m = await self.get_file_data(ctx, path)
-            actor = m.get_current()
-            if actor['name'] == data[1]:
-                await ctx.send(f'You can\'t remove characters on their turn!')
-                return
-            try:
-                if m.actors.index(m.get_actor(data[1])) < m.turn:
-                    m.turn -= 1
-                m.remove_actors(data[1])
-            except ValueError:
-                await ctx.send(f'`{data[1]}` has not been found')
-                return
-            self.save_update(path, m)
-            await self.update_message(ctx, m)
-            await ctx.send(f'`{data[1]}` has been removed from combat')
-
-        elif data[0] == 'next':
-            path = f'{main.folder}{ctx.message.channel.id}.json'
-            m = await self.get_file_data(ctx, path)
-            actor = m.get_current()
-            if ctx.message.author.id == actor['author'] or ctx.message.author.guild_permissions.administrator:
-                m.next_turn()
-                actor = m.get_current()
-                self.save_update(path, m)
-                await self.update_message(ctx, m)
-                await ctx.send(f"**Initiative {actor['initiative']} (round {m.round}):** {actor['name']} (<@{actor['author']}>)```\n{actor['name']}```")
-            else:
-                await ctx.send('It is not your turn!')
+            await ctx.send(f"**Initiative {actor['initiative']} (round {m.round}):** {actor['name']} (<@{actor['author']}>)```\n{actor['name']}```")
+        else:
+            await ctx.send('It is not your turn!')
 
     async def update_message(self, ctx, combat: combat_data.Combat):
         msg = await ctx.fetch_message(combat.message)
