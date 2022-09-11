af8525b03fa42ab1209d3040d6c27dbc8f0af140
Xentellion <d.g.mchedlishvili@gmail.com>
2022-09-11 16:55:51 +0300

    New load system

--- __main__.py
+++ __main__.py
@@ -1,25 +1,35 @@
 import os
+import yaml
 import asyncio
 import discord.errors
-from dotenv import load_dotenv
 from discord.ext import commands
 
-# Get loading data
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-PREFIX = os.getenv('BOT_PREFIX')
+class EmptyConfig(Exception):
+    def __init__(self, config_path: str):
+        self.message = f'Please, set up variables in {config_path}'
+        super().__init__(self.message)
 
 intents = discord.Intents.all()
 intents.members = True
    
 folder = './Data/'
 server_folder = 'Server/'
+config_path = folder + 'config.yml'
+with open(config_path, 'a+', encoding="utf8") as file:
+    file.seek(0)
+    config = yaml.safe_load(file)
+    if type(config) is not dict:
+        yaml.dump({"BOT_PREFIX": "", "DISCORD_TOKEN": ""}, file)
+        raise EmptyConfig(config_path)
+    elif config['BOT_PREFIX'] == "":
+        raise EmptyConfig(config_path)
+
 if not os.path.exists(folder + server_folder):
     os.makedirs(folder + server_folder)
 
 activity = discord.Activity(type=discord.ActivityType.watching, name = "как горят миры")
 status = discord.Status.online
-bot = commands.Bot(command_prefix=PREFIX, intents=intents, activity = activity)
+bot = commands.Bot(command_prefix=config['BOT_PREFIX'], intents=intents, activity = activity)
 
 @bot.event
 async def on_ready():
@@ -29,8 +39,6 @@ async def main():
     for f in os.listdir('./Cogs'):
         if f.endswith('.py'):
             await bot.load_extension(f'Cogs.{f[:-3]}')
-    await bot.start(TOKEN)
+    await bot.start(config['DISCORD_TOKEN'])
 
 asyncio.run(main())
-
-bot.run(TOKEN, bot=True, reconnect=True)
