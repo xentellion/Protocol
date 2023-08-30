8b16e4eb0cfc88efe2543921066018b4f0c10ce2
Xentellion <d.g.mchedlishvili@gmail.com>
2023-08-30 18:39:42 +0300

    roll reroll

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# PROTOCOL
+[![python](https://img.shields.io/badge/Python-3.10.12-3776AB.svg?style=flat&logo=python&logoColor=white)](https://www.python.org)
+
+# THE PROTOCOL
+
+Protocol is definitely a bot.
+
+## 
--- src/Cogs/roll_dice.py
+++ src/Cogs/roll_dice.py
@@ -1,5 +1,4 @@
 import re
-from math import floor, ceil
 from random import randint
 from discord.ext import commands
 from discord import errors
@@ -16,64 +15,61 @@ class RollDice(commands.Cog):
         self.bot = bot
 
     @commands.command(name='roll')
-    async def roll_send(self, ctx, *, i):
+    async def roll_send(self, ctx, *, input_query: str):
         try:
             await ctx.message.delete()
         except errors.NotFound:
             print('Message has been deleted\n')
             return
-# --------Separate commentary and remove spaces
-        body = i
-        nb_rep = 1
-        while (nb_rep):
-            (body, nb_rep) = re.subn(regex_brac, '1d20', body)
-        res = re.sub(regex_comment, '', body)
-        body = i.replace(res, '')
-# --------Separate dices, roll and save data
-        split = re.split(regex_dice, body)
+# --------Separate commentary and strip spaces
+        comment = re.sub(regex_comment, '', input_query).strip()
+        expression = input_query.replace(comment, '').strip()
+# --------Separate dices and roll them
+        split = re.split(regex_dice, expression)
         split = [spl.replace('*', '\*') for spl in split]
-        spl = split[0]
-        results = []
-        if len(split) <= 1:
-            await ctx.send(f'No dices found in "{i}"')
-            return
-        for j in range(1, len(split)-1, 2):
-            dices = split[j].split('d')
-            for idx, k in enumerate(dices):
-                flt = float(k)
-                dices[idx] = floor(flt) if flt > 1 else ceil(flt)
-            split[j] = f'{dices[0]}d{dices[1]}'
+
+        text_result = []
+        roll_result = []
+        for j in range(1, len(split), 2):
+            dices = list(map(int, split[j].split('d')))
             if dices[0] > 100:
                 await ctx.send('Too many dices in one throw')
                 return
-            rand = [randint(1, dices[1]) for k in range(dices[0])]
-            demo = '('
+            split[j] = f'{dices[0]}d{dices[1]}'
+            rand = [randint(1, dices[1]) for _ in range(dices[0])]
+            roll_result.append(rand)
+
+            demo_roll = []
             for k in rand:
                 crit = '**' if k == dices[1] or k == 1 else ''
-                demo += f'{crit}{str(k)}{crit}, '
-            demo = f'{demo[:-2]})'
-            spl += split[j] + demo + split[j+1]
-            results.append(rand)
-# --------Calculate
-        if len(results) > 1:
-            for j in results:
-                body = re.sub(regex_dice, str(sum(j)), body, 1)
+                demo_roll.append(f'{crit}{str(k)}{crit}')
             try:
-                summa = round(eval(body), 2)
+                text_result.append(f"{split[j]}({', '.join(demo_roll)}){split[j+1]}")
+            except IndexError:
+                await ctx.send(f'No dices found in "{input_query}"')
+                return
+# --------Calculate rolls
+        # one dice - calclulates all rolls individually
+        # multile - sums all up
+        if len(roll_result) > 1:
+            for j in roll_result:
+                expression = re.sub(regex_dice, str(sum(j)), expression, 1)
+            try:
+                summa = round(eval(expression), 2)
             except SyntaxError:
                 await ctx.send("I can't calculate that!")
                 return
         else:
-            body = [re.sub(regex_dice, str(j), body, 1) for j in results[0]]
+            expression = [re.sub(regex_dice, str(roll), expression, 1) for roll in roll_result[0]]
             try:
-                body = [round(eval(j), 2) for j in body]
+                expression = [round(eval(j), 2) for j in expression]
             except SyntaxError:
                 await ctx.send("I can't calculate that!")
                 return
-            summa = body[0] if len(body) == 1 else tuple(body)
+            summa = expression[0] if len(expression) == 1 else tuple(expression)
 # --------Result
-        res = 'Result' if res == '' else re.sub(regex_cursive, '*', res)
-        message = f'{ctx.message.author.mention} 🎲\n**{res}:** {spl} \n**Total: **{summa}\n'
+        comment = 'Result' if comment == '' else re.sub(regex_cursive, '*', comment)
+        message = f"{ctx.message.author.mention} 🎲\n**{comment}:** {''.join(text_result)} \n**Total: **{summa}\n"
         if len(message) > 1950:
             await ctx.send('The answer is too long')
             return
